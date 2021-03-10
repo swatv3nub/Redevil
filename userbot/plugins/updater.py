f134e83b47679593cd02d82fe128e7d54a71dfb5
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-10 16:26:31 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -67,9 +67,9 @@ HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
-GIT_REPO_NAME = "LEGEND-BOT"
+GIT_REPO_NAME = "Devil2.0"
 heroku_api = "https://api.heroku.com"
-UPSTREAM_REPO_URL="https://github.com/legendx22/LEGEND-BOT"
+UPSTREAM_REPO_URL="https://github.com/lucifeermorningstar/Devil2.0"
 
 # ================= CONSTANT =================
 JAVES_NNAME = str(HEROKU_APPNAME)

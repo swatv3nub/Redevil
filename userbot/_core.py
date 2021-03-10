a0a8e77266efb34268780c8bd7b9ea8181de707e
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-10 12:49:19 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -9,8 +9,8 @@ from userbot.utils import admin_cmd, load_module, remove_plugin, sudo_cmd
 from userbot.utils import edit_or_reply as eor
 
 DELETE_TIMEOUT = 3
-thumb_image_path = "./Resources/IMG_20201211_144821_071.jpg"
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOT"
+thumb_image_path = "./Resources/IMG_20210304_093322_763.jpg"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Devil BOT"
 
 
 @bot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
@@ -89,10 +89,10 @@ async def unload(event):
     shortname = event.pattern_match["shortname"]
     try:
         remove_plugin(shortname)
-        qwe = await eor(event, f"LEGEND BOT Has Successfully unloaded {shortname}")
+        qwe = await eor(event, f"Devil BOT Has Successfully unloaded {shortname}")
     except Exception as e:
         await qwe.edit(
-            "LEGEND BOT has Successfully unloaded {shortname}\n{}".format(shortname, str(e))
+            "Devil BOT has Successfully unloaded {shortname}\n{}".format(shortname, str(e))
         )
 
 
@@ -111,5 +111,5 @@ async def load(event):
         qwe = await eor(event, f"Successfully loaded {shortname}")
     except Exception as e:
         await qwe.edit(
-            f"LEGEND BOT could not load {shortname} because of the following error.\n{str(e)}"
+            f"devil BOT could not load {shortname} because of the following error.\n{str(e)}"
         )

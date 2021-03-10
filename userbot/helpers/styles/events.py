f3526d4602d2db5dfd435166b4906a69c51a5c76
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-10 12:30:55 +0530

    Update events.py

--- userbot/helpers/styles/events.py
+++ userbot/helpers/styles/events.py
@@ -86,7 +86,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "[LEGENDBOT Official Chat](https://t.me/teamishere)"
+                    link = "[Deviluserbot Official Chat](https://t.me/deviluserbot)"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
                     text += "Nothing is logged except the fact of error and date\n"

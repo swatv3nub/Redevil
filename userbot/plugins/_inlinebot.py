08b71d7387aa452706a340609e77426f53ebaa64
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-11 13:51:40 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -94,7 +94,7 @@ async def cmd_list(event):
 
         else:
 
-            help_string = f"""Userbot Helper.. Provided by ð±{DEFAULTUSER}ð± \n
+            help_string = f"""Userbot Helper.. Provided by Devil±{DEFAULTUSER}ð± \n
 
 Userbot Helper to reveal all the commands\nDo .help plugin_name for commands, in case popup doesn't appear."""
 

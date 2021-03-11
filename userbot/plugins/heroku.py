0213d988ddb3fa3eec319fe154cdff22af61b77a
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-11 14:54:14 +0530

    Update heroku.py

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -176,7 +176,7 @@ async def _(dyno):
             dyno.chat_id,
             "logs.txt",
             reply_to=dyno.id,
-            caption="LEGEND BOT logs of 100+ lines",
+            caption="𝙳𝙴𝚅𝙸𝙻 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙻𝙾𝙶𝚂 𝚘𝙵 100+ lines",
         )
         
         await asyncio.sleep(5)
